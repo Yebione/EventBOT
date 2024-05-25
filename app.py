12f67f38f3895165923776bd8ca7efcec7d7921e
Yebione <kylebillones.63@gmail.com>
2024-05-25 20:31:57 +0800

    included .env to gitignore

--- app.py
+++ app.py
@@ -21,14 +21,8 @@ def get_gemini_response(question):
     return response.text
 
 def main():
-    questions = [
-        "What is your event name?",
-        "When will it be held?",
-        "Where is the venue of the event?",
-        "Who are the target participants?",
-        "What are the expected activities in the said event (separate using comma)?",
-    ]
 
+    # Streamlit User Interface styling  
     st.markdown("""
         <style>
         .reportview-container {
@@ -44,13 +38,13 @@ def main():
         }
         </style>
         """, unsafe_allow_html=True)
-
+    
+    # Streamlit Sidebar Header 
     st.sidebar.title('📝 EventBOT')
     st.sidebar.write('Tired of planning and structuring events? Let EventBOT help you!')
-
     st.sidebar.write('')
 
- 
+    # Streamlit Sidebar Inputs (Guide questions for the prompt)
     event_input = st.sidebar.text_input("What is the name of your event?")
     date_input = st.sidebar.text_input("When will it be held? (Month-Day-Year)")
     venue_input = st.sidebar.text_input("Where is the venue of the said event?")
@@ -59,6 +53,7 @@ def main():
     st.sidebar.write('')
     st.sidebar.write('')
 
+    # Streamlit Additional Model Instructions (Multi-Level Prompting)
     ask_question = "Make me an event plan, used the following as your input." + "Event Name:"+ event_input + ". Date:" + date_input + ". Venue " + venue_input + " Participants: " + participant_input + " Activities: " + activities_input + ""
     instruction_1= "Structure it this order, Event name, date, venue, participants, overview (in paragraph form), program flow, objectives, needed materials or resources to prepare (provide price in PHP if possible, add a total too, present in a table), conclusion."
     instruction_2= "Layout it well and make it presentable."
@@ -67,18 +62,12 @@ def main():
     instruction_5 = "Write a program flow of the event. (Time - activity description - person in charge (if applicable), present in a table"
     input = " "+ask_question+" "+ instruction_1+" "+instruction_2+" "+instruction_3+" "+instruction_4+" "+instruction_5+" "
 
-
-
+    # Streamlit Submit Button and Response Display (Event Plan Design)
     submit_button = st.sidebar.button('Submit')
     if submit_button:
         response=get_gemini_response(input)
         st.subheader("Hi, I'm EventBOT! Here's the event design I made for you.")
         st.write(response)
-       
-
-
-    
- 
 
 if __name__ == "__main__":
     main()
